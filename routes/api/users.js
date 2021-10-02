059677a0fceff2186cb8b40e25d0325af1dd45b2
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-02 22:22:06 +0530

    Login Implemented at endpoint /api/users/login

--- routes/api/users.js
+++ routes/api/users.js
@@ -47,6 +47,36 @@ router.post("/register", async (req, res, next) => {
     }
 })
 
+router.post("/login", async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(400).send({
+                message: "All fields are required"
+            })
+        }
+        const user = await User.findOne({ email });
+        if (user == null) {
+            return res.status(401).send({
+                message: "User does not exist"
+            });
+        } else {
+            let result = await bcrypt.compare(password, user.password)
+
+            if(result) {
+                user.success = true
+                return res.status(200).send(user)
+            }
+            else {
+                return res.status(401).send({
+                    message: "Incorrect password"
+                })
+            }
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
 
 
 
