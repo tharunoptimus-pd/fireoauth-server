3db45263458e155d8b82c081215cf60c8e595b09
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-03 20:29:22 +0530

    Installed CORS

--- app.js
+++ app.js
@@ -3,12 +3,19 @@ dotenv.config()
 
 const express = require("express")
 const app = express()
+const cors = require("cors")
 const mongoose = require("./database")
 const port = process.env.PORT || 3003
 
 const server = app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
 })
+
+app.use(cors({
+    origin: '*'
+}))
+
+app.options('*', cors())
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
