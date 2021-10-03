9123c5a6783199dd6cc8af9756b54c315eb15e50
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-03 23:29:05 +0530

    Created tokens API

---
+++ routes/api/tokens.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const bcrypt = require("bcrypt");
+const router = express.Router();
+const cryptoString = require('crypto-string')
+const User = require("../../schemas/UserSchema");
+const Token = require("../../schemas/TokenSchema");
+const API = require("../../schemas/APISchema");
+const Client = require("../../schemas/ClientSchema");
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended: true}));
+
+
+module.exports = router;
