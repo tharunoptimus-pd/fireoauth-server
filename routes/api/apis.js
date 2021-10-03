3a90488ddeb040e2973adc10627d45e59da9c042
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-03 14:38:47 +0530

    Created Login endpoint for client

--- routes/api/apis.js
+++ routes/api/apis.js
@@ -48,4 +48,36 @@ router.post("/register", async (req, res, next) => {
     }
 })
 
+router.post("/login", async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        if(!email || !password) {
+            return res.status(400).send({
+                message: "All fields are required"
+            })
+        }
+        const client = await Client.findOne({ email });
+        if (client == null) {
+            return res.status(401).send({
+                message: "User does not exist"
+            });
+        } else {
+            let result = await bcrypt.compare(password, client.password)
+
+            if(result) {
+                client.success = true
+                return res.status(200).send(client)
+            }
+            else {
+                return res.status(401).send({
+                    message: "Incorrect password"
+                })
+            }
+        }
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+
 module.exports = router;
