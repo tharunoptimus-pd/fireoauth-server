50dc850d577dc317770a59d72d765cd2ee29022d
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-04 20:18:56 +0530

    Removed unwanted fields from API schema

--- schemas/APISchema.js
+++ schemas/APISchema.js
@@ -3,11 +3,6 @@ const Schema = mongoose.Schema;
 const APISchema = new Schema({
 
 	domainName: { type: String, required: true },
-	needFirstName: { type: Boolean, default: false },
-	needLastName: { type: Boolean, default: false },
-	needEmail: { type: Boolean, default: false },
-	needProfilePic: { type: Boolean, default: false },
-	requestedTransactions: { type: Number, default: 0 },
 	transactionNumbers: { type: Number, default: 0 }
 
 }, { timestamps: true });
