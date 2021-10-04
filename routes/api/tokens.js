e648f46a625a57f1f7ce8481e032de50840b3a11
Tharunraj Optimus <tharunoptimus@outlook.com>
2021-10-04 14:36:23 +0530

    Created Token Generation POST endpoint

--- routes/api/tokens.js
+++ routes/api/tokens.js
@@ -12,5 +12,28 @@ const Client = require("../../schemas/ClientSchema");
 app.use(express.json())
 app.use(express.urlencoded({extended: true}));
 
+router.post("/generate", async (req, res) => {
+
+    let sessionId = req.body.sessionId
+    let userId = req.body.userId
+
+    if(! sessionId || ! userId) return res.status(400).send({message: "Missing sessionId or userId"})
+
+    let user = await User.findById(userId)
+
+    if(user == null) return res.status(401).send({ message: "User does not exist" })
+
+    let newToken = {
+        sessionId: sessionId,
+        firstName: user.firstName,
+        lastName: user.lastName,
+        email: user.email,
+        profilePic: user.profilePic
+    }
+
+    let token = await Token.create(newToken)
+
+    res.status(201).send({ tokenId: token._id })
+})
 
 module.exports = router;
