f8787bf338d19aea0bcd0353af03d3aa6610291d
tharunoptimus-pd <81877822+tharunoptimus-pd@users.noreply.github.com>
2023-10-06 19:14:36 +0530

    Change Dice Bear API usage

--- routes/api/users.js
+++ routes/api/users.js
@@ -36,7 +36,7 @@ router.post("/register", async (req, res, next) => {
                 lastName,
                 email,
                 password,
-                profilePic: `https://avatars.dicebear.com/api/adventurer-neutral/${firstName}${lastName}.svg?backgroundColor=variant03`
+                profilePic: `https://api.dicebear.com/7.x/fun-emoji/svg?radius=50&backgroundColor=4cf0c3&mood=[happy]&seed=${firstName}.${lastName}`
             }
 
             let newUser = await User.create(newUserData)
